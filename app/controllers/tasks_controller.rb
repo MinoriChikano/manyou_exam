c9ff41d4769afcec94839e2dc835bdaea7275f42
minori chikano <bcloudabc@gmail.com>
2023-06-15 00:24:44 +0900

    詳細画面の作成

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,25 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to new_task_path
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:task_name, :detail)
+  end
+
+end
