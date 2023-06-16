ea9488a9aaf3801e33a298454c3ae928d252275f
minori chikano <bcloudabc@gmail.com>
2023-06-17 06:54:39 +0900

    expired_atをタスクに追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_15_025320) do
+ActiveRecord::Schema.define(version: 2023_06_16_212352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2023_06_15_025320) do
     t.text "detail"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "expired_at", default: -> { "now()" }, null: false
   end
 
 end
