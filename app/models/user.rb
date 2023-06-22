dd8b01167353f2a8104df218bc913abf09e7ec21
minori chikano <bcloudabc@gmail.com>
2023-06-22 14:32:27 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :tasks
   has_secure_password
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }
   validates :password, length: { minimum: 4 }
 end
