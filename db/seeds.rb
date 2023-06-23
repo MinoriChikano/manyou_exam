398c7380476f43363fdeb9e47919c57fa0cecfd2
minori chikano <bcloudabc@gmail.com>
2023-06-23 12:48:57 +0900

    新規登録したらroot画面へリダイレクトする機能実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
 User.create!(
-  name: 'ダンブルドア',
-  email: 'potter@test.com',
+  name: 'グリフィンドール',
+  email: 'potter1@test.com',
   password: '123456',
   password_confirmation: '123456',
+  admin: true
+)
+
+User.create!(
+  name: 'スリザリン',
+  email: 'yamada@test.com',
+  password: '222222',
+  password_confirmation: '222222',
   admin: true
 )
