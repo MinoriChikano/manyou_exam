016d6044761bca940be2f0449b8bcaa8c34acd6b
minori chikano <bcloudabc@gmail.com>
2023-06-26 00:34:12 +0900

    管理者権限の所有者が一人の場合、編集したり削除できなくする

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,20 @@
 class User < ApplicationRecord
+  before_update :non_updatable_admin
+  before_destroy :undeletable_admin
+
   has_many :tasks, dependent: :destroy
   has_secure_password
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }
   validates :password, length: { minimum: 4 }
+
+  private
+
+  def non_updatable_admin
+    throw :abort if User.where(admin: :true).count == 1 && self.admin == false
+  end
+
+  def undeletable_admin
+    throw :abort if User.where(admin: :true).count == 1
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,6 @@
 User.create!(
-  name: 'グリフィンドール',
-  email: 'potter1@test.com',
-  password: '123456',
-  password_confirmation: '123456',
-  admin: true
-)
-
-User.create!(
-  name: 'スリザリン',
-  email: 'yamada@test.com',
+  name: 'ニッカ',
+  email: 'nicka@test.com',
   password: '222222',
   password_confirmation: '222222',
   admin: true
