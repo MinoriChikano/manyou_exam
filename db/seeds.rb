b60eb04e8651f6a42a2c9e61a7a345be992a98a0
minori chikano <bcloudabc@gmail.com>
2023-06-28 17:32:12 +0900

    シードデータ投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,21 @@
-Label.create!(
-  name: "保留"
+User.create!(
+  name: "管理者",
+  email: "admin@test.com",
+  password: "123456"
+  admin: true
 )
+
+10.times do |n|
+  name: "シードくん#{n + 1}"
+  email: "seed#{n + 1}@test.com"
+  password: "123456"
+  
+  User.create!(name: name, email: email, password: password, admin:false)
+end
+
+10.times do |n|
+  name = "label#{n}"
+
+  Label.create!(name: name)
+end
+
